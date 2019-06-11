a084a3fdbc0ec5c3befdcd270dc20566e646eb60
rami072 <44778825+rami072@users.noreply.github.com>
2019-06-11 16:28:02 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+  console.log("about to look for routes!!!")
+  //console.dir(req.headers)
+  next()
+});
+
+
+app.get('/', function(req, res, next) {
+  res.render('index',{title:"Express Demo"});
+});
+
+
+app.get('/SignUp', function(req, res, next) {
+  res.render('SignUp',{title:"SignUp"});
+});
+
+app.use(function(req,res,next){
+  console.log("about to look for post routes!!!")
+  next()
+});
+
+function processSignUp(req,res,next){
+  res.render('postsign',
+     {title:"postsign",});
+}
+
+app.post('/postsign',processSignUp);
+
+// app.use('/', indexRouter);  // this is how we use a router to handle the / path
+// but here we are more direct
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
